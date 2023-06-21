b6ad999bf12e794270099f7aeba223c3a32a7e9b
tungtt30 <tungchipsctn2@gmail.com>
2023-06-21 20:14:27 +0700

    init

---
+++ Main.java
@@ -0,0 +1,52 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(new FileInputStream("input.txt"));
+		System.out.println("Processing...");
+		Workbook workbook = new HSSFWorkbook();
+		Sheet sheet = workbook.createSheet("Output");
+		int rowNum = 1;
+		while (scanner.hasNext()) {
+			String input = scanner.nextLine();
+			if (input == "")
+				continue;
+			String[] sentences = input.split("\\.");
+			for (String sentence : sentences) {
+				Row row = sheet.createRow(rowNum++);
+				Cell cell = row.createCell(0);
+				if (sentence.trim().isEmpty()) {
+					continue;
+				}
+				cell.setCellValue(cell.getStringCellValue() + sentence.trim() + ".");
+				if (sentence.endsWith(".")) {
+					rowNum++;
+					row = sheet.createRow(rowNum);
+					cell = row.createCell(0);
+				}
+				rowNum = rowNum + 2;
+			}
+		}
+		try {
+			FileOutputStream outputStream = new FileOutputStream("output.xls");
+			workbook.write(outputStream);
+			workbook.close();
+			outputStream.close();
+			System.out.println("Done !");
+		} catch (IOException e) {
+			System.out.println("Error " + e);
+		}
+		scanner.close();
+	}
+}
